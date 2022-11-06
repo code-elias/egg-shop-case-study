7e5e8bc66b29039c09515df3f494e829003bd15c
EliGolam <elistasibusiness@gmail.com>
2022-11-06 16:13:54 +0100

    Init

---
+++ src/components/PrototypePage.vue
@@ -0,0 +1,54 @@
+<!-- Noi useremo il classico OPTIONS API per Vue che abbiamo imparato invece del COMPOSITION API nuovo -->
+
+
+<!-- NON CANCELLARE O MODIFICARE QUESTA PAGINA -->
+<template>
+  <div>
+    <!-- HTML -->
+  </div>
+</template>
+
+
+
+<script>
+// IMPORTS
+
+  export default {
+    // JAVASCRIPT 
+    data() {
+      return {
+
+      }
+    },
+
+    props: {
+
+    },
+
+    computed: {
+      
+    },
+
+    created() {
+
+    }, 
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    }, 
+
+    components: {
+
+    }
+  }
+</script>
+
+
+
+<style lang="scss" scoped>
+  // Quando possibile, usiamo @import con un partials invece di scrivere il scss direttamente qui sotto
+</style>
