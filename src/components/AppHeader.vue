2f189e5aa67e864af455b8ffb5f98ec8b11c6623
EliGolam <elistasibusiness@gmail.com>
2022-11-07 17:17:35 +0100

    Added egg-shop-logo to the page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,24 @@
 <template>
   <header>
-    <h1>Page Name</h1>
+    
+    <div class="emg-logo-container">
+      <img src="@/assets/img/egg-shop-logo.png" alt="" class="emg-img-fluid emg-shop-logo">
+    </div>
+
   </header>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        
+      }
+    },
+
+    methods: {
+      
+    }
   }
 </script>
 
