3aafd270fb302110bf131c59fed90c98f518e75d
EliGolam <elistasibusiness@gmail.com>
2022-11-07 17:28:30 +0100

    Added simple comments to the code

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,11 @@
 <template>
   <header>
-    
+    <!-- SHOP LOGO -->
     <div class="emg-logo-container">
       <img src="@/assets/img/egg-shop-logo.png" alt="Egg Shop Logo" class="emg-img-fluid emg-shop-logo">
     </div>
 
+    <!-- NavBar containing the router links to the various pages -->
     <NavBarComponent />
 
   </header>
