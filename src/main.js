0b8cf2d76257f859dead3af83859c66f98ccec7b
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-11 18:21:45 +0100

    import fontawesome icons

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import router from './router'
 
 import './assets/styles/app.scss'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faTwitter, faInstagram)
+
 const app = createApp(App)
 
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
