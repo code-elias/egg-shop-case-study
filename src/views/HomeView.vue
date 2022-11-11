180c155aca7037c9d859e2b03e8d7d8971f5d0ec
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-11 16:58:23 +0100

    add <p>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-    <h1>Home Page</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+  <h1>Home Page</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui doloremque
+    officia sapiente sunt voluptas tempora exercitationem voluptatum possimus
+    quam! Commodi!/p>
+  </p>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
   
   
